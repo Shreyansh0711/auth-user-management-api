29aec475e64d0486f4d738bbed523b6f0e9b969a
Shreyansh <shreyansh7234@gmail.com>
2026-06-10 13:23:44 +0530

    cloudinary added

---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"//filesystem
+(async function() {
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret:process.env.CLOUDINARY_API_SECRET
+    });
+})();
+
+const uploadoncloudinary= async(localfilepath)=>{
+    try {
+        if(!localfilepath)return null;
+        const response=await cloudinary.uploader.upload(localfilepath,{resource_type:"auto"})
+        console.log("file uploaded successfully",
+            response.url
+        )
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localfilepath)//remove locally saved temp file as the upload operation get failed
+    }
+}
+
+
+  export {uploadoncloudinary}
