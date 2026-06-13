259925ad28c7cf185f7c74880a2525b308e0720d
Shreyansh <shreyansh7234@gmail.com>
2026-06-13 20:38:58 +0530

    routes added

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,4 +12,8 @@ app.use(express.urlencoded({extended:true,limit:"16kb"})
 app.use(express.static("public")) 
 app.use(cookieparser())
 app.use(express.json({limit:"16kb"}))
+//import routes
+import userrouter from "./routes/user.routes.js"
+// routes declearation
+app.use("/api/v1/users",userrouter)
 export{app}
